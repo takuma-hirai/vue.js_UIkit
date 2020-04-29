0a9b58198e894b16aefbaece0e7f8b1c3c07a7b9
takuma-hirai <takuma.hirai1@gmail.com>
2020-04-30 04:42:55 +0900

    larabel

---
+++ my-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
